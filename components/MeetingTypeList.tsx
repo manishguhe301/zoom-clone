66a88422a5894916f310713e64ce39c2432fa13c
Manish <manishguhe301.com>
2024-12-06 14:23:30 +0530

    feat: Join Meeting feature

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -8,6 +8,7 @@ import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
 import { useToast } from '@/hooks/use-toast';
 import { Textarea } from './ui/textarea';
 import DatePicker from 'react-datepicker';
+import { Input } from './ui/input';
 
 const MeetingTypeList = () => {
   const [meetingState, setMeetingState] = useState<
@@ -160,6 +161,21 @@ const MeetingTypeList = () => {
         buttonText='Start Meeting'
         handleClick={createMeeting}
       />
+
+      <MeetingModal
+        isOpen={meetingState === 'isJoiningMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title='Enter Meeting Link'
+        className='text-center'
+        buttonText='Start Meeting'
+        handleClick={() => router.push(values.link)}
+      >
+        <Input
+          placeholder='Meeting Link'
+          className='border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0'
+          onChange={(e) => setValues({ ...values, link: e.target.value })}
+        />
+      </MeetingModal>
     </section>
   );
 };
